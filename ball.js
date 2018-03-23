5a90d2d0ab345adf1637e4b8359d208ca58c8bac
mariuspenciu <penciumarius@gmail.com>
2018-03-23 17:58:57 +0200

    ball 4

--- ball.js
+++ ball.js
@@ -9,7 +9,7 @@ function deseneaza()
 {
 	
 	var t = document.getElementById("id_transform");
-	t.setAttribute("translation", "0" + y + "0");
+	t.setAttribute("translation", "0 " + y + " 0");
 	y += directie;
 	if (y > 10)
 		directie = -1;
