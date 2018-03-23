aa8a690c16e3480b4b3a5e1aca949c72cca16432
mariuspenciu <penciumarius@gmail.com>
2018-03-23 17:50:25 +0200

    bila miscatoare

---
+++ ball.js
@@ -0,0 +1,18 @@
+
+setInterval(deseneaza, 30);
+var y = 0;
+var directie = 1;
+
+function deseneaza()
+
+{
+	
+	var t = document.getElementById("id_transform");
+	t.setAttribute("translation", "0" + y + "0");
+	y += directie;
+	if (y > 10)
+		directie = -1;
+	else
+		if(y < 0)
+			directie = 1;
+}
