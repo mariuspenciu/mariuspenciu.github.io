047b0d65b1335bc61c2720f8f29bacd3116c5409
mariuspenciu <penciumarius@gmail.com>
2018-03-23 18:18:39 +0200

    ball6

--- ball.js
+++ ball.js
@@ -1,18 +1,20 @@
 document.getElementById("id_bussines_level").innerHTML = "Bussines version = 2018.03.23.1";
 
-setInterval(deseneaza, 30, 0, 1);
+var stare = {y:0, directie: 1};
+
+setInterval(deseneaza, 30, 0, stare);
 
 
-function deseneaza()
+function deseneaza(x)
 
 {
 	
 	var t = document.getElementById("id_transform");
-	t.setAttribute("translation", "0 " + y + " 0");
-	y += directie;
-	if (y > 10)
-		directie = -1;
+	t.setAttribute("translation", "0 " + x.y + " 0");
+	x.y +=x.directie;
+	if (x.y > 10)
+		x.directie = -1;
 	else
-		if(y < 0)
-			directie = 1;
+		if(x.y < 0)
+			x.directie = 1;
 }
